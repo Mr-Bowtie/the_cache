785cc06622f324b09f3727a485e4ad54d2ef783e
Ian Withers <mr.bowtie989@gmail.com>
2024-08-05 07:58:17 -0400

    WIP: adventure log routes and views

--- app.rb
+++ app.rb
@@ -6,9 +6,11 @@ require 'sinatra/activerecord'
 require 'dotenv/load'
 require 'pry'
 Dir[File.join(__dir__, 'models', '*.rb')].each { |file| require file }
+
 Dotenv.load('.env')
 
 set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
+
 # Create records directories if they doesn't exist
 Dir.mkdir('./records') unless Dir.exist?('./records')
 Dir.mkdir('./records/npcs') unless Dir.exist?('./records/npcs')
@@ -32,8 +34,38 @@ get '/npcs/:title' do
 end
 
 get '/adventure_logs' do
-  @adventure_logs = Dir.entries('./records/adventure_logs').reject { |f| File.directory? f }
+  # @adventure_logs = Dir.entries('./records/adventure_logs').reject { |f| File.directory? f }
+  # erb :adventure_logs
+
+  @adventure_logs = AdventureLog.all
   erb :adventure_logs
+end
+
+get '/adventure_logs/new' do
+  binding.pry
+  erb :'adventure_logs/new', locals: { errors: nil }
+end
+
+get '/adventure_logs/:id' do
+  @adventure_log = AdventureLog.find(params[:id])
+  erb :'adventure_logs/show'
+end
+
+post '/adventure_logs/create' do
+  # define exactly what param elements we want to use to prevent any other value being set by users
+  @adventure_log = AdventureLog.new(
+    title: params[:title],
+    date: params[:date],
+    content: params[:content]
+  )
+
+  # TODO: handle save failure
+  if @adventure_log.save
+    redirect "/adventure_logs/#{@adventure_log.id}"
+
+  else
+    erb :'adventure_logs/new', locals: { errors: @adventure_log.errors }
+  end
 end
 
 get '/adventure_logs/:title' do
@@ -67,16 +99,17 @@ get '/edit_record/:type/:title' do
 end
 
 post '/create_file' do
-  file_path = "./records/#{params[:type]}/#{params[:title]}.txt"
-  File.open(file_path, 'w') do |file|
-    file.write(params[:content])
-  end
-  redirect "/#{params[:type]}"
+  # file_path = "./records/#{params[:type]}/#{params[:title]}.txt"
+  # File.open(file_path, 'w') do |file|
+  #   file.write(params[:content])
+  # end
+  # redirect "/#{params[:type]}"
 end
 
-post '/edit_file' do 
-  unless (params[:old_title] == params[:title]) 
-    File.rename("./records/#{params[:type]}/#{params[:old_title]}.txt", "./records/#{params[:type]}/#{params[:title]}.txt")
+post '/edit_file' do
+  unless params[:old_title] == params[:title]
+    File.rename("./records/#{params[:type]}/#{params[:old_title]}.txt",
+                "./records/#{params[:type]}/#{params[:title]}.txt")
   end
 
   file_path = "./records/#{params[:type]}/#{params[:title]}.txt"
