605e975a8db007704eeeb30fe78101b47102d6b4
Ian Withers <mr.bowtie989@gmail.com>
2024-08-05 22:52:18 -0400

    feat: validate session_number for adventure_logs

--- models/adventure_logs.rb
+++ models/adventure_logs.rb
@@ -3,4 +3,5 @@ require 'sinatra/activerecord'
 class AdventureLog < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :date, presence: true
+  validates :session_number, uniqueness: true, numericality: true, allow_blank: true, allow_nil: true
 end
